876388debaf88cd6bbc484669251f1c9ee781b1e
ZhangWuren <954489951@qq.com>
2018-11-27 09:15:55 +0800

    1

--- sudoku.cpp
+++ sudoku.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+class CRaw{ //数独的一行
+    private:
+    unsigned short __raw[9];
+    public:
+    CRaw();
+    void SwitchRaw(int switchNumber);
+
+};
+
 int main(int argc, char *argv[])
 {
     int sudokuNumber = 0;
