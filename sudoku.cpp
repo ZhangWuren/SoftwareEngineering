3c5a03467d6a4eff356b80f785199cc6c1915664
ZhangWuren <954489951@qq.com>
2018-11-27 14:50:55 +0800

    1

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,21 +1,29 @@
 #include "iostream"
+#include "cstdio"
 using namespace std;
 
 class CRow
 { //数独的一行
   private:
     unsigned short __row[9];
+    unsigned short __select[8];
 
   public:
-    CRow(int Rownumber);
-    void SwitchRow(int switchNumber);
-    void PrintRow();
-
     CRow(int Rownumber)
     {
         __row[0] = 6;
-        
-    }
+        for (int i = 0; i < 8; i++)
+        {
+            if (i < 5)
+            {
+                __select[i] = i + 1;
+            }
+            else
+            {
+                __select[i] = i + 2;
+            }
+        }
+    };
 
     void PrintRow()
     {
@@ -24,11 +32,13 @@ class CRow
             cout << __row[i] << " ";
         }
         cout << endl;
-    }
+    };
 };
 
 int main(int argc, char *argv[])
 {
     int sudokuNumber = 0;
     cin >> sudokuNumber;
+    CRow row(1);
+    return 0;
 }
