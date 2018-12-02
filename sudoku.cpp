2d502b5ccbc4d2c85ccc7b02479f01550147922c
ZhangWuren <954489951@qq.com>
2018-12-02 19:07:40 +0800

    1

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,44 +1,93 @@
 #include "iostream"
-#include "cstdio"
+#include "algorithm"
 using namespace std;
 
 class CRow
 { //数独的一行
   private:
     unsigned short __row[9];
-    unsigned short __select[8];
+    unsigned short __trow[9];
 
   public:
-    CRow(int Rownumber)
+    CRow()
     {
         __row[0] = 6;
-        for (int i = 0; i < 8; i++)
+        __trow[0] = 6;
+        for (int i = 1; i < 9; i++)
         {
-            if (i < 5)
+            if (i < 6)
             {
-                __select[i] = i + 1;
+                __row[i] = i;
+                __trow[i] = i;
             }
             else
             {
-                __select[i] = i + 2;
+                __row[i] = i + 1;
+                __trow[i] = i + 1;
             }
         }
     };
-
-    void PrintRow()
-    {
-        for (int i = 0; i < 9; i++)
-        {
-            cout << __row[i] << " ";
-        }
-        cout << endl;
-    };
+    void NextRow();
+    void TranslateAndPrintRow(int transNumber);
 };
+
+void CRow::NextRow()
+{
+    next_permutation(__row + 1, __row + 9);
+}
+
+void CRow::TranslateAndPrintRow(int transNumber)
+{
+    for (int i = 0; i < 9; i++)
+    {
+        __trow[(i + transNumber) % 9] = __row[i];
+    }
+    for (int i = 0; i < 9; i++)
+    {
+        cout << __trow[i] << " ";
+    }
+    cout << endl;
+}
 
 int main(int argc, char *argv[])
 {
     int sudokuNumber = 0;
+    int TranslateArray1[3] = {1, 4, 7};
+    int TranslateArray2[3] = {2, 5, 8};
+    CRow crow;
+
     cin >> sudokuNumber;
-    CRow row(1);
+    for (int i = 0; i <= sudokuNumber / 36; i++)
+    {
+        int Times = 0;
+        if (i < sudokuNumber / 36)
+        {
+            Times = 36;
+        }
+        else
+        {
+            Times = sudokuNumber % 36;
+        }
+        for (int j = 0; j < Times; j++)
+        {
+            crow.TranslateAndPrintRow(0);
+            crow.TranslateAndPrintRow(3);
+            crow.TranslateAndPrintRow(6);
+            crow.TranslateAndPrintRow(TranslateArray1[0]);
+            crow.TranslateAndPrintRow(TranslateArray1[1]);
+            crow.TranslateAndPrintRow(TranslateArray1[2]);
+            crow.TranslateAndPrintRow(TranslateArray2[0]);
+            crow.TranslateAndPrintRow(TranslateArray2[1]);
+            crow.TranslateAndPrintRow(TranslateArray2[2]);
+            if (j % 6 == 5)
+            {
+                next_permutation(TranslateArray1, TranslateArray1 + 3);
+            }
+            next_permutation(TranslateArray2, TranslateArray2 + 3);
+            cout << endl;
+        }
+        crow.NextRow();
+    }
+
     return 0;
 }
